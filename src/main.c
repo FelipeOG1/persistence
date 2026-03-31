31e18cd1da815c463ef9a639563689608c6ef5d2
FelipeOG1 <feliog1234@gmail.com>
2026-03-30 18:35:51 -0600

    new buffer entry

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_len;
+    size_t input_len;
+
+} InputBuffer;
 
 
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer)); 
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_len = 0;
+    input_buffer->input_len = 0;
+    
+    return input_buffer;
+}
 int main() {
-    std::vector<char> buffer(1024, 0);
-    for ( ;; ) {
-        
-        
-    }
+    printf("%d", x);
     return 0;
 }
+
+
