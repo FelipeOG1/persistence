eadc8628cb39fd6ad14a6b6fcda3af8a92c30e84
FelipeOG1 <feliog1234@gmail.com>
2026-03-30 19:25:27 -0600

    handle commands

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
 typedef struct {
     char* buffer;
     size_t buffer_len;
@@ -26,7 +28,11 @@ void read_input(InputBuffer* buffer) {
     }
     buffer->input_len = bytes_read - 1;
     buffer->buffer[bytes_read - 1] = 0;//eliminate /n character
-   
+}
+
+//commands handlers
+bool is_command(InputBuffer* input_buffer) {
+    return input_buffer->buffer[0] == '.';
 }
 
 void print_db() { printf("db >"); }
@@ -35,19 +41,25 @@ void close_input_buffer(InputBuffer* buffer) {
     free(buffer->buffer);
     free(buffer);
 }
+
+
 void start_repl() {
     for (;;) {
         
         InputBuffer* input_buffer = new_input_buffer();
         print_db();
         read_input(input_buffer);
-        if (strcmp(input_buffer->buffer, "exit") == 0) {
-            close_input_buffer(input_buffer);
-            break;
-        }
-        
-        print_buffer(input_buffer);
 
+        if (is_command(input_buffer)) {
+            if (strcmp(input_buffer->buffer, ".exit") == 0) {
+                close_input_buffer(input_buffer);
+                break;
+            } else {
+               printf("NOT VALID COMMAND\n");
+            }
+   
+        }
+            
     }
 }
 int main() {
